15520b9c09c0d6db896b8716d4e2183a1e90f152
en-cetus <2334740207@qq.com>
2026-09-04 11:13:29 +0000

    try initial ring shift logic using MPI_Sendrecv

--- part1a.c
+++ part1a.c
@@ -181,8 +181,8 @@ int main(int argc, char* argv[]) {
          memcpy(pos + recv_owner * loc_n, recv_buf, loc_n * sizeof(vect_t));
 
          /* Prepare next transmission block */
-         memcpy(send_buf, recv_buf, loc_n * sizeof(vect_t));
-         send_owner = recv_owner;
+         // memcpy(send_buf, recv_buf, loc_n * sizeof(vect_t));
+         // send_owner = recv_owner;
       }
 #     ifndef NO_OUTPUT
       if (step % output_freq == 0)
